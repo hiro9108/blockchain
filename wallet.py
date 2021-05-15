def56fd5cb19fe89967e66357b4dbe495203498b
hiro9108 <hiroshi.8.egawa@gmail.com>
2021-05-15 10:00:40 -0700

    create signature

--- wallet.py
+++ wallet.py
@@ -5,6 +5,8 @@ import hashlib
 from ecdsa import NIST256p
 from ecdsa import SigningKey
 
+import utils
+
 class Wallet(object):
 
     def __init__(self):
@@ -63,9 +65,36 @@ class Wallet(object):
         # blockchain_address = base58.b58encode(binascii.unhexlify(address_hex)).decode('utf-8')
         blockchain_address = base58.b58encode(address_hex).decode('utf-8')
         return blockchain_address
+    
+class Transaction(object):
+
+    def __init__(self, sender_private_key, sender_public_key,
+                sender_blockchain_address, recipient_blockchain_address, value):
+        self.sender_private_key = sender_private_key
+        self.sender_public_key = sender_public_key
+        self.sender_blockchain_address = sender_blockchain_address
+        self.recipient_blockchain_address = recipient_blockchain_address
+        self.value = value
+
+    def generate_signature(self):
+        sha256 = hashlib.sha256()
+        transaction = utils.sorted_dict_by_key({
+            'sender_blockchain_address': self.sender_blockchain_address,
+            'recipient_blockchain_address': self.recipient_blockchain_address,
+            'value': float(self.value)
+        })
+        sha256.update(str(transaction).encode('utf-8'))
+        message = sha256.digest()
+        private_key = SigningKey.from_string(bytes().fromhex(self.sender_private_key), curve=NIST256p)
+        private_key_sign = private_key.sign(message)
+        signature = private_key_sign.hex()
+        return signature
 
 if __name__ == '__main__':
     wallet = Wallet()
     print(wallet.private_key)
     print(wallet.public_key)
     print(wallet.blockchain_address)
+    t = Transaction(
+        wallet.private_key, wallet.public_key, wallet.blockchain_address, 'B', 1.0)
+    print(t.generate_signature())
